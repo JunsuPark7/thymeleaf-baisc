878733b793986db044ba81032f7ca28a6f146f4d
junsu <wnstn9618@gmail.com>
2024-02-02 06:30:38 +0900

    init commit

---
+++ src/test/java/hello/thymelefe/ThymelefeBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymelefe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymelefeBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
